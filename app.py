fc074c740599955290b1d2fdb30f40868cdac712
Vasiluță Mihai-Alexandru <alexv@siluta.ro>
2020-04-11 21:01:55 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import markovify
+import requests
+import flask
+app = flask.Flask(__name__)
+
+
+@app.route("/api/<username>")
+def thing(username):
+    req = requests.get(
+        f"https://www.allmytweets.net/get_tweets.php?count=200&tweet_mode=extended&trim_user&screen_name={username}")
+
+    text = ''
+    for tweet in req.json():
+        text += tweet['full_text'] + "\n"
+
+    mark = markovify.NewlineText(text)
+    sent = mark.make_sentence()
+    if sent == None:
+        sent = "Too few tweets to imitate"
+    return sent
+
+
+@app.route("/")
+def serveMain():
+    return flask.render_template('index.html')
