52befe5b220560ae8307fc0f7781660d2f7fe3dc
HYPERDRIVE-Motivator <zain-tech@outlook.com>
2020-04-13 21:50:09 -0500

    enhanced theme swapper

---
+++ static/docs.js
@@ -0,0 +1,12 @@
+console.warn("HELLO!")
+var y = document.getElementsByTagName("prismLink")
+const x = window.matchMedia("(prefrs-color-scheme: dark)")
+
+if (x == true) { // If media query matches
+  y.setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/themes/prism-tomorrow.min.css");
+  console.warn("SWAPPED THEME")
+} else {
+  y.setAttribute("href", "https://cdn.jsdelivr.net/npm/prismjs@1.20.0/themes/prism.css")''
+  console.error("THEME NOT CHANGED")
+  console.error(x)
+}
