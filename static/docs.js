c3c1ba0dea35bc2894dae5b3ce979b7430b04da6
HYPERDRIVE-Motivator <zain-tech@outlook.com>
2020-04-14 00:23:07 -0500

    fixed dynamic css

--- static/docs.js
+++ static/docs.js
@@ -1,12 +1,16 @@
-console.warn("HELLO!")
-var y = document.getElementsByTagName("prismLink")
-const x = window.matchMedia("(prefrs-color-scheme: dark)")
+var elementCheck = document.getElementById("#backgroundDivCheck");
+var stylingValue = getComputedStyle(elementCheck);
+var trueBackgroundValue = stylingValue.backgroundColor
+console.log(trueBackgroundValue);
+console.log(document.getElementById("prismLink"))
+//
 
-if (x == true) { // If media query matches
-  y.setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/themes/prism-tomorrow.min.css");
+//rgb(255, 255, 254)
+if (trueBackgroundValue == "rgb(15, 15, 15)") { // If media query matches
+  document.getElementById("prismLink").setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/themes/prism-tomorrow.min.css");
   console.warn("SWAPPED THEME")
 } else {
-  y.setAttribute("href", "https://cdn.jsdelivr.net/npm/prismjs@1.20.0/themes/prism.css")''
+  y.setAttribute("href", "https://cdn.jsdelivr.net/npm/prismjs@1.20.0/themes/prism.css");
   console.error("THEME NOT CHANGED")
   console.error(x)
 }
